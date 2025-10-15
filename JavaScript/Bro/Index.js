8e4679be262cb33574baaacf70e754c1c0139355
Morrison <mc_lmorrison27@mcjvs2.org>
2025-10-15 09:58:18 -0400

    first message

---
+++ JavaScript/Bro/Index.js
@@ -0,0 +1,18 @@
+//console.log('Hello World');
+//console.log('PIZZA');
+
+//window.alert('This is an alert');
+//window.alert('i like pizza');
+
+
+document.getElementById('myh1').textContent = 'Hello Wrld!';
+document.getElementById('myP').textContent = 'I like pizza!';
+
+// This is a comment
+
+/* This
+ is
+ a
+multi-line
+ comment
+  */
